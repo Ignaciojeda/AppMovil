45d7f714d0e442be0d07dce70ec850d18f0306fa
Ignaciojeda <ig.ojedao@duocuc.cl>
2024-11-04 09:08:22 -0300

    Android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://rphpxwnpbulwhnhaapeu.supabase.co',  // Asegúrate de tener el valor correcto
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwaHB4d25wYnVsd2huaGFhcGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MzMzODMsImV4cCI6MjA0NTIwOTM4M30.w8vhc96ABfuEytJS3yt_e4k0WtgG-k-2iG4tiBWLbEc'               // Asegúrate de tener el valor correcto
 };
